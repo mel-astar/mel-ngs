c86df2e5539fc2b2fe6d9dcc082cc8f75fb8179e
ganguvamshi <ganguvamshi@gmail.com>
2012-07-31 14:32:04 +0800

    added scripts

---
+++ scripts/fasta_appendchr.pl
@@ -0,0 +1,17 @@
+use strict;
+use warnings;
+
+use Bio::SeqIO;
+
+die("\n\tThis script is for appending \"chr\" after > in fasta file\n\tMake sure to isntall BioPerl\n\t \
+     \tUsage :$0 <input fasta> <output fasta> \n\n") if($#ARGV<1);
+
+my $in=Bio::SeqIO->new(-file=>"<$ARGV[0]",-format=>"fasta");
+my $out=Bio::SeqIO->new(-file=>">$ARGV[1]",-format=>"fasta");
+
+while(my $entry=$in->next_seq){
+ my $id = $entry->display_id;
+ $id="chr$id";
+ $entry->display_id($id);
+ $out->write_seq($entry);
+}
