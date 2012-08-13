c1f254a88ca4f13f8175dc4fb4a05d426a3c71d7
ganguvamshi <ganguvamshi@gmail.com>
2012-08-13 15:52:00 +0800

    changes for Casava

---
+++ mel-rnaseq/Casava_BaseCalling/readfq.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/env perl
+## This program gives the total no of fastq records and cumulative length of their sequence and qualities in entire Fastq file
+
+use strict;
+use warnings;
+
+sub readfq {
+ my ($fh, $aux) = @_;
+ @$aux = [undef, 0] if (!defined(@$aux));
+ return if ($aux->[1]);
+ if (!defined($aux->[0])) {
+  while (<$fh>) {
+   chomp;
+   if (substr($_, 0, 1) eq '>' || substr($_, 0, 1) eq '@') {
+    $aux->[0] = $_;
+    last;
+   }
+  }
+  if (!defined($aux->[0])) {
+   $aux->[1] = 1;
+   return;
+  }
+ }
+
+ my $name = /^.(\S+)/? $1 : '';
+ my $seq = '';
+ my $c;
+ $aux->[0] = undef;
+ while (<$fh>) {
+  chomp;
+  $c = substr($_, 0, 1);
+  last if ($c eq '>' || $c eq '@' || $c eq '+');
+  $seq .= $_;
+ }
+
+ $aux->[0] = $_;
+ $aux->[1] = 1 if (!defined($aux->[0]));
+ return ($name, $seq) if ($c ne '+');
+ my $qual = '';
+ while (<$fh>) {
+  chomp;
+  $qual .= $_;
+  if (length($qual) >= length($seq)) {
+    $aux->[0] = undef;
+    return ($name, $seq, $qual);
+  }
+ }
+
+ $aux->[1] = 1;
+ return ($name, $seq);
+}
+
+my @aux = undef;
+my ($name, $seq, $qual);
+my ($n, $slen, $qlen) = (0, 0, 0);
+while (($name, $seq, $qual) = readfq(\*STDIN, \@aux)) {
+++$n;
+$slen += length($seq);
+$qlen += length($qual) if ($qual);
+}
+print join("\t", $n, $slen, $qlen), "\n";
