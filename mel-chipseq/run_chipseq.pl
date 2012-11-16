67bc8e84bbb713e371d47353fa404ec526c825a9
Gangu Vamshi <vamshi@mel01.storage.acrc>
2012-11-16 14:27:05 +0800

    appended chipseq run

--- mel-chipseq/run_chipseq.pl
+++ mel-chipseq/run_chipseq.pl
@@ -1,19 +1,24 @@
 use Config::General;
 my $CONFIG_FILE = $ARGV[0] || "Config.txt";
-my $conf = Config::General->new(-ConfigFile=>"Config.txt");
+my $conf = Config::General->new(-ConfigFile=>$CONFIG_FILE);
 my %config = $conf->getall;
-
-my $INPUT = $config{'INPUTFASTQ'};
+my $ONLYTREAT = $config{'ONLYTREAT'};
+my $INPUT = $config{'INPUTFASTQ'} if($ONLYTREAT<1);
 my $TREAT = $config{'TREATMENTFASTQ'};
 my $PROJ = $config{'PROJECTNAME'};
 my $OUTDIR = $config{'OUTPUTDIR'};
+my $INPUTBAM = $config{'INPUTBAM'};
 my $OUTDIR_PROJ = "$OUTDIR/$PROJ"."_dir";
 my $BOWTIE_DIR = "$OUTDIR_PROJ/bowtie";
 my $MACS_DIR = "$OUTDIR_PROJ/macs";
 my $MOTIF_DIR = "$OUTDIR_PROJ/motif";
+my $ANNOTATEPEAKS_DIR = "$OUTDIR_PROJ/annotated_peaks";
 my $GENOME = $config{'GENOME'};
 $GENOME=~/^(\w{2})/;
 my $MACS_GENOME = $1 || die "can't determine MACs genome";
+if($MACS_GENOME eq 'hg'){
+	$MACS_GENOME = 'hs';
+}
 my $BOWTIE_PARAMS = $config{'BOWTIE_PARAMS'} || "-m 1 --chunkmbs 256 --best -p 7";
 my $MACS_PARAMS = $config{'MACS_PARAMS'} || "-p 1e-5";
 
@@ -24,31 +29,46 @@ unless (-d $OUTDIR_PROJ) {
 	mkdir $OUTDIR_PROJ;
 }
 mkdir "$OUTDIR/$PROJ"."_dir"."/bowtie";
+mkdir "$OUTDIR/$PROJ"."_dir"."/annotated_peaks";
 mkdir "$OUTDIR/$PROJ"."_dir"."/macs";
 mkdir "$OUTDIR/$PROJ"."_dir"."/motif";
 
 #run bowtie
 print STDERR "mel-chipseq: Running bowtie\n\n";
 chdir  $BOWTIE_DIR;
-my $BOWTIE_IN_OUTFILE = $PROJ."_INPUT";
+my $BOWTIE_IN_OUTFILE = $PROJ."_INPUT" if($ONLYTREAT<1);
 my $BOWTIE_TREAT_OUTFILE = $PROJ."_TREAT";
-my $bowtie_IN_cmd = "(bowtie $BOWTIE_PARAMS -S $GENOME $INPUT |samtools view -ut -bS - | samtools sort - $BOWTIE_IN_OUTFILE) 2> $OUTDIR_PROJ/log.txt";
+my $bowtie_IN_cmd = "(bowtie $BOWTIE_PARAMS -S $GENOME $INPUT |samtools view -ut -bS - | samtools sort - $BOWTIE_IN_OUTFILE) 2> $OUTDIR_PROJ/log.txt" if($ONLYTREAT<1);
 my $bowtie_TREAT_cmd = "(bowtie $BOWTIE_PARAMS -S $GENOME $TREAT |samtools view -ut -bS - | samtools sort - $BOWTIE_TREAT_OUTFILE) 2>> $OUTDIR_PROJ/log.txt";
 
-system("$bowtie_IN_cmd");
+if($ONLYTREAT<1){
+  system("$bowtie_IN_cmd") unless $INPUTBAM; #do need to run input alignment if INPUTBAM is provided.
+  $INPUTBAM = $INPUTBAM || "$BOWTIE_DIR/$BOWTIE_IN_OUTFILE.bam";
+}
 # || die ("problems running $bowtie_IN_cmd");
 system("$bowtie_TREAT_cmd");
 # || die ("problems running $bowtie_TREAT_cmd");
-print STDERR "mel-chipseq: "."$bowtie_IN_cmd\n";
+print STDERR "mel-chipseq: "."$bowtie_IN_cmd\n" unless($ONLYTREAT>0);
 print STDERR "mel-chipseq: "."$bowtie_TREAT_cmd\n";
 
 #run MACS
 print STDERR "mel-chipseq: Running MACS\n\n";
 chdir "$MACS_DIR";
-my $macs_cmd = "macs14 $MACS_PARAMS -c $BOWTIE_DIR/$BOWTIE_IN_OUTFILE.bam -t $BOWTIE_DIR/$BOWTIE_TREAT_OUTFILE.bam -f BAM -g $MACS_GENOME -n $PROJ 2>> $OUTDIR_PROJ/log.txt";
+if($ONLYTREAT<1){
+ my $macs_cmd = "macs14 $MACS_PARAMS -c $INPUTBAM -t $BOWTIE_DIR/$BOWTIE_TREAT_OUTFILE.bam -f BAM -g $MACS_GENOME -n $PROJ 2>> $OUTDIR_PROJ/log.txt";
+}
+else{
+ my $macs_cmd = "macs14 $MACS_PARAMS -t $BOWTIE_DIR/$BOWTIE_TREAT_OUTFILE.bam -f BAM -g $MACS_GENOME -n $PROJ 2>> $OUTDIR_PROJ/log.txt";
+} 
 print STDERR "mel-chipseq: ".$macs_cmd."\n";
 system($macs_cmd);
-# || die ("problems running $macs_cmd");
+ # || die ("problems running $macs_cmd");
+
+#run annotated peaks
+chdir "$ANNOTATEPEAKS_DIR";
+print STDERR "mel-chipseq: Running annotatePeaks.pl\n\n";
+my $annotate_cmd = "annotatePeaks.pl $MACS_DIR/$PROJ"."_peaks.bed $GENOME > $PROJ"."_annotated_peaks.txt";
+system($annotate_cmd);
 
 
 #run findmotifs
